6e52acc717f92dbd7e9926296f04b2d9573813bf
Andrei Silviu Dragnea <andreisilviudragnea@gmail.com>
2021-03-13 20:41:47 +0200

    Annotation import example
    wip
    
    Revert "wip"
    
    This reverts commit ad4048ff
    
    Rename .java to .kt
    
    wip

---
+++ src/main/java/com/example/main/Main.java
@@ -0,0 +1,16 @@
+package com.example.main;
+
+import com.example.Value;
+
+public class Main {
+
+    private final String field;
+
+    public Main(@Value String field) {
+        this.field = field;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
