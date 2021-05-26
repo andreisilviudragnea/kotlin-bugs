38f301f4dcb3b2d84304081fbd353d2e1f54354e
Andrei Silviu Dragnea <andreisilviudragnea@gmail.com>
2021-05-26 16:47:07 +0300

    More complex example

--- src/main/java/com/example/kotlin_superclass/JavaSubclass.java
+++ src/main/java/com/example/kotlin_superclass/JavaSubclass.java
@@ -1,8 +1,15 @@
 package com.example.kotlin_superclass;
 
+import com.example.kotlin_superclass.another.ExtensionsKt;
+import com.example.kotlin_superclass.another.More;
+
 public class JavaSubclass extends SuperClass {
 
     public String getValue() {
+        ExtensionsKt.getExtension("");
+        ExtensionsKt.function("");
+        More.INSTANCE.function2("");
+        More.function3("");
         return getField();
     }
 }
---
+++ src/main/java/com/example/kotlin_superclass/another/Extensions.kt
@@ -0,0 +1,12 @@
+package com.example.kotlin_superclass.another
+
+object More {
+  fun String.function2() = ""
+
+  @JvmStatic
+  fun String.function3() = ""
+}
+
+fun String.function() = ""
+
+val String.extension get() = ""
