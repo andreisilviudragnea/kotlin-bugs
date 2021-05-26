b64334baec24cd52220de7b2b04eea07e88c690d
Andrei Silviu Dragnea <andreisilviudragnea@gmail.com>
2021-05-26 12:28:58 +0300

    Kotlin superclass bug

---
+++ src/main/java/com/example/kotlin_superclass/JavaSubclass.java
@@ -0,0 +1,8 @@
+package com.example.kotlin_superclass;
+
+public class JavaSubclass extends SuperClass {
+
+    public String getValue() {
+        return getField();
+    }
+}
---
+++ src/main/java/com/example/kotlin_superclass/SuperClass.kt
@@ -0,0 +1,5 @@
+package com.example.kotlin_superclass
+
+open class SuperClass {
+  var field: String = ""
+}
