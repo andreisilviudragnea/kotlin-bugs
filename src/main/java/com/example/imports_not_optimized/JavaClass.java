285892214244fde7fac6a4ed5af4ec6533d120e6
Andrei Silviu Dragnea <andreisilviudragnea@gmail.com>
2021-05-27 18:37:27 +0300

    Imports not optimized new example

---
+++ src/main/java/com/example/imports_not_optimized/JavaClass.java
@@ -0,0 +1,8 @@
+package com.example.imports_not_optimized;
+
+public class JavaClass extends KotlinClass {
+
+    public String method() {
+        return getField1() + getField2();
+    }
+}
---
+++ src/main/java/com/example/imports_not_optimized/KotlinClass.kt
@@ -0,0 +1,6 @@
+package com.example.imports_not_optimized
+
+open class KotlinClass {
+  val field1: String = ""
+  val field2: String = ""
+}
